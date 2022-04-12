e50e38e3c94d4b154471495698a67f9eab11240b
Backia Raj Kanagaraj <backiaraj.kanagaraj@syncfusion.com>
2022-04-12 18:15:48 +0530

    EJ2MVC-424-Prepared getting started sample for ASP.NET MVC Avatar, AccumulationChart, AutoComplete, Barcode, Breadcrumb and Badge controls

---
+++ AutoComplete/ASP.NET MVC Razor Examples/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AutoCompleteSample.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            List<string> data = new List<string>() { "Badminton", "Basketball", "Cricket", "Football", "Golf", "Gymnastics", "Hockey", "Tennis" };
+            return View(data);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
