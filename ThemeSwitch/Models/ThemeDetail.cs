3dd1aaab987a9383c65a4183c41078930fc574b3
Balaji-Elumalai <balaji.elumalai@syncfusion.com>
2022-04-12 08:28:07 +0530

    ASP.NET MVC documentation samples committed.

---
+++ ThemeSwitch/Models/ThemeDetail.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ThemeSwitch.Models
+{
+    public class ThemeDetail
+    {
+        public string ID { get; set; }
+        public string Text { get; set; }
+
+        public List<ThemeDetail> ThemeDetails()
+        {
+            List<ThemeDetail> themes = new List<ThemeDetail>();
+            themes.Add(new ThemeDetail() { ID = "material", Text = "Material" });
+            themes.Add(new ThemeDetail() { ID = "bootstrap", Text = "Bootstrap" });
+            themes.Add(new ThemeDetail() { ID = "fabric", Text = "Fabric" });
+            themes.Add(new ThemeDetail() { ID = "bootstrap4", Text = "Bootstrap 4" });
+            themes.Add(new ThemeDetail() { ID = "tailwind", Text = "TailWind" });
+            themes.Add(new ThemeDetail() { ID = "tailwind-dark", Text = "TailWind Dark" });
+            themes.Add(new ThemeDetail() { ID = "material-dark", Text = "Material Dark" });
+            themes.Add(new ThemeDetail() { ID = "bootstrap-dark", Text = "Bootstrap Dark" });
+            themes.Add(new ThemeDetail() { ID = "fabric-dark", Text = "Fabric Dark" });
+            themes.Add(new ThemeDetail() { ID = "highcontrast", Text = "High Contrast" });
+            return themes;
+        }
+
+    }
+}
