635ff522fc3fd7eab8723aff9392ef3bdcf9c1c9
Balaji-Elumalai <balaji.elumalai@syncfusion.com>
2022-04-13 11:52:27 +0530

    EJ2MVC-421 - ASP.NET MVC UG samples committed.

---
+++ Localization/Localization-with-static-culture/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Localization_with_static_culture
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
