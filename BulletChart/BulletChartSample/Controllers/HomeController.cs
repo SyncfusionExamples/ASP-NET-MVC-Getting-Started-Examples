61441a271defcb3249be28a3cb5d58bab6b7f4a9
Backia Raj Kanagaraj <backiaraj.kanagaraj@syncfusion.com>
2022-04-21 12:53:21 +0530

    Removed unwanted file and added new samples for MVC controls

---
+++ BulletChart/BulletChartSample/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BulletChartSample.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            List<BulletChartData> data = new List<BulletChartData>
+            {
+                new BulletChartData { value = 270, target = 250}
+            };
+            return View(data);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+    public class BulletChartData
+    {
+        public double target;
+        public double value;
+    }
+}
