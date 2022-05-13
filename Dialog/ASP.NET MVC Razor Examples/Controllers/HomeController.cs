0e452af42ab1a91397a44f38eea32886a9ea8638
Backia Raj Kanagaraj <backiaraj.kanagaraj@syncfusion.com>
2022-05-13 18:21:24 +0530

    Prepared getting started samples for ASP.NET MVC controls

---
+++ Dialog/ASP.NET MVC Razor Examples/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DialogSample.Controllers
+{
+    public class HomeController : Controller
+    {
+        public class ButtonModel
+        {
+            public string content { get; set; }
+            public bool isPrimary { get; set; }
+            public string cssClass { get; set; }
+
+        }
+        public ActionResult Index()
+        {
+            ViewBag.DialogButtons1 = new ButtonModel() { isPrimary = true, cssClass = "e-flat", content = "OK" };
+            ViewBag.DialogButtons2 = new ButtonModel() { content = "Cancel", cssClass = "e-flat" };
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
