b61f6ac1d0fd11ff46253d484b704268224eea9b
SaravanaPriya31 <Saravana31>
2023-09-27 16:15:14 +0530

    849973-standalone

---
+++ PDFViewer/ASP.NET MVC Razor Examples - Standalone PDF Viewer/App_Start/BundleConfig.cs
@@ -0,0 +1,16 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace PDFViewerSample
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.min.css",
+                      "~/Content/site.css"));
+        }
+    }
+}
