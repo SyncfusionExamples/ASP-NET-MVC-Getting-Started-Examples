ce4fc851b906497e524ca3fdf6641c1d821ee4d4
Uma-SF4267 <umamageshwari.shanmugam@syncfusion.com>
2024-09-19 12:37:22 +0530

    896384: Added validatePassword action to get whether document is password protected.

--- PDFViewer/ASP.NET MVC Razor Examples/App_Start/FilterConfig.cs
+++ PDFViewer/ASP.NET MVC Razor Examples/App_Start/FilterConfig.cs
@@ -8,6 +8,7 @@ namespace PDFViewerSample
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            filters.Add(new JsonMaxLengthAttribute());
         }
     }
 }
---
+++ PDFViewer/ASP.NET MVC Razor Examples/App_Start/JsonMaxLengthFilterConfig.cs
@@ -0,0 +1,18 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace PDFViewerSample
+{
+    public class JsonMaxLengthAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            var jsonResult = filterContext.Result as JsonResult;
+            if (jsonResult != null)
+            {
+                jsonResult.MaxJsonLength = int.MaxValue;  // Set maximum length
+            }
+            base.OnActionExecuting(filterContext);
+        }
+    }
+}
