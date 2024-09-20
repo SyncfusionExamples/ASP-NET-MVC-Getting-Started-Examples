ed3df8f6c9d41184693565154780558cf94430bc
Uma-SF4267 <umamageshwari.shanmugam@syncfusion.com>
2024-09-20 12:46:03 +0530

    896384: commit

--- PDFViewer/ASP.NET MVC Razor Examples/App_Start/FilterConfig.cs
+++ PDFViewer/ASP.NET MVC Razor Examples/App_Start/FilterConfig.cs
@@ -8,7 +8,6 @@ namespace PDFViewerSample
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
-            filters.Add(new JsonMaxLengthAttribute());
         }
     }
 }
